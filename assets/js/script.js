da97eb9b21759dbf1aba91346fe7505a01034ec0
Ankit Somani <ankit.m.somani@gmail.com>
2021-03-09 21:50:48 -0500

    creating variables for current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,32 @@
-// 1. display current time //
+
+var currentTime = $("current-time")
+
+
+// 1. DISPLAY CURRENT TIME //
+// var check = moment(n.entry.date_entered, 'YYYY/MM/DD');
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+var months = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+
+var date = new Date();
+var d = days[date.getDay()]+" , "+months[date.getMonth()]+" "+date.getDate();
+currentTime.text(d);
+
+
 
 // 2. display past, present or future time by color // 
 // a. if past time, show gray //
 // b. if present time, show red //
 // c. if future time, show green //
 
+
+
+
+
+
 // 3. save text entered in timeblock to local storage //
-// a. saved events stay
+// a. saved events stay after refreshing page //
 
 
 
@@ -14,8 +34,6 @@
 
 
 
-
-d
 
 
 
@@ -43,11 +61,11 @@ d
 // check for button clicks inside an on click
 
 
-$(".saveBtn").on("click", function () {
-    var value = $(this).siblings(".task").val();
-    var time = $(this).parent().attr("id")
-    localStorage.setItem(time, value)
-})
+// $(".saveBtn").on("click", function () {
+//     var value = $(this).siblings(".task").val();
+//     var time = $(this).parent().attr("id")
+//     localStorage.setItem(time, value)
+// })
 
 
 // write a function to update the hours in the time blocks                             
@@ -71,6 +89,6 @@ $(".saveBtn").on("click", function () {
 
 // load any saved data from local storage
 
-$("hour-9 .task").val(localStorage.getItem("hour-9"));
+// $("hour-9 .task").val(localStorage.getItem("hour-9"));
 
 // display current day on page (use moment)
